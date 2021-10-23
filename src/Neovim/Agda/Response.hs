13e6bdc772ced9da875eb21b6afa2ae7f5289d3e
0xd34df00d <0xd34df00d@gmail.com>
2021-10-23 16:20:51 -0500

    Leverage DerivingVia to avoid duplicating JSON derivations

--- src/Neovim/Agda/Response.hs
+++ src/Neovim/Agda/Response.hs
@@ -1,41 +1,55 @@
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
 
 module Neovim.Agda.Response where
 
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Char8 as BS
 import Data.Int (Int64)
-import GHC.Generics (Generic)
+import GHC.Generics (Generic (Rep))
+
+newtype AgdaJson a = AgdaJson { getAgdaJson :: a }
+  deriving (Generic)
+
+agdaJsonOpts :: A.Options
+agdaJsonOpts = A.defaultOptions { A.sumEncoding = A.TaggedObject "kind" "", A.fieldLabelModifier = takeWhile (/= '\'') }
+
+instance (A.GFromJSON A.Zero (Rep a), Generic a) => A.FromJSON (AgdaJson a) where
+  parseJSON = fmap AgdaJson . A.genericParseJSON agdaJsonOpts
+
+instance (A.GToJSON A.Zero (Rep a), Generic a) => A.ToJSON (AgdaJson a) where
+  toJSON = A.genericToJSON agdaJsonOpts . getAgdaJson
 
 data StatusInfo = StatusInfo
   { checked :: Bool
   , showIrrelevantArguments :: Bool
   , showImplicitArguments :: Bool
-  } deriving (Show, Generic, A.FromJSON, A.ToJSON)
+  }
+  deriving (Show, Generic, A.FromJSON, A.ToJSON)
 
 data DisplayInfo = AllGoalsWarnings
   { visibleGoals :: [()]
   , warnings :: [()]
   , invisibleGoals :: [()]
   , errors :: [()]
-  } deriving (Show, Generic)
-
-instance A.FromJSON DisplayInfo where
-  parseJSON = A.genericParseJSON A.defaultOptions { A.sumEncoding = A.TaggedObject "kind" "" }
-
-instance A.ToJSON DisplayInfo where
-  toJSON = A.genericToJSON A.defaultOptions { A.sumEncoding = A.TaggedObject "kind" "" }
+  }
+  deriving (Show, Generic, A.FromJSON, A.ToJSON)
 
 data HlBit = HlBit
   { atoms :: [String]
   , range :: [Int64]
-  } deriving (Show, Generic, A.FromJSON, A.ToJSON)
+  }
+  deriving (Show, Generic, A.FromJSON, A.ToJSON)
 
 data HlInfo = HlInfo
   { remove :: Bool
   , payload :: [HlBit]
-  } deriving (Show, Generic, A.FromJSON, A.ToJSON)
+  }
+  deriving (Show, Generic, A.FromJSON, A.ToJSON)
 
 data Response
   = Status { status :: StatusInfo }
@@ -46,12 +60,7 @@ data Response
   | ClearRunningInfo
   | ClearHighlighting
   deriving (Show, Generic)
-
-instance A.FromJSON Response where
-  parseJSON = A.genericParseJSON A.defaultOptions { A.sumEncoding = A.TaggedObject "kind" "", A.fieldLabelModifier = takeWhile (/= '\'') }
-
-instance A.ToJSON Response where
-  toJSON = A.genericToJSON A.defaultOptions { A.sumEncoding = A.TaggedObject "kind" "", A.fieldLabelModifier = takeWhile (/= '\'') }
+  deriving (A.FromJSON, A.ToJSON) via (AgdaJson Response)
 
 decodeResponse :: BS.ByteString -> Either String Response
 decodeResponse = A.eitherDecodeStrict'
