30672f1358bf211c1a201d563a78bb8f6706841b
0xd34df00d <0xd34df00d@gmail.com>
2021-10-23 12:12:47 -0500

    Support different insertion strategies

--- src/Neovim/Agda/Input.hs
+++ src/Neovim/Agda/Input.hs
@@ -11,6 +11,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Control.Monad
 import Data.Maybe
+import Data.String.Interpolate.IsString
 import UnliftIO
 
 import Neovim
@@ -28,8 +29,8 @@ sampleTrie = Trie.fromList [ ("all", "∀")
 data Cursor = Cursor { row :: Int, col :: Int }
   deriving (Eq, Ord, Show)
 
-getCurColI :: Neovim env Cursor
-getCurColI = do
+getCursorI :: Neovim env Cursor
+getCursorI = do
   (r, c) <- nvim_win_get_cursor =<< nvim_get_current_win
   pure $ Cursor (fromIntegral r) (fromIntegral c - 1)
   -- -1 because the cursor points to the next symbol after insertion
@@ -45,7 +46,7 @@ maybeStartSymbol :: Neovim AgdaEnv ()
 maybeStartSymbol = do
   line <- nvim_get_current_line
   unless (BS.null line) $ do
-    curCol <- col <$> getCurColI
+    curCol <- col <$> getCursorI
     case line `BS.index` curCol of
          '`' -> asks symbolInputCol >>= \var -> atomically $ writeTVar var (Just curCol)
          _   -> pure ()
@@ -59,7 +60,7 @@ split3 str start len = (left, mid, right)
 handleNextSymbol :: Int -> Neovim AgdaEnv ()
 handleNextSymbol start = do
   line <- nvim_get_current_line
-  Cursor { .. } <- getCurColI
+  Cursor { .. } <- getCursorI
   if col >= start
   then do
     let (left, mid, right) = split3 line start (col - start + 1)
@@ -67,11 +68,19 @@ handleNextSymbol start = do
          Nothing  -> pure ()
          Just sub -> do
            let subBytes = T.encodeUtf8 sub
-           nvim_set_current_line $ left <> subBytes <> right
-           win <- nvim_get_current_win
-           nvim_win_set_cursor win (fromIntegral row, fromIntegral $ start + BS.length subBytes)
+           insertBehaviour <- nvim_eval [i|get(g:, 'insert_behaviour', '')|]
+           case insertBehaviour of
+                ObjectString "undo" -> insertPreservingUndo left subBytes right row
+                _                   -> insertPreservingHighlights subBytes col
            cancelInput
   else cancelInput
+  where
+    insertPreservingUndo left subBytes right row = do
+      nvim_set_current_line $ left <> subBytes <> right
+      win <- nvim_get_current_win
+      nvim_win_set_cursor win (fromIntegral row, fromIntegral $ start + BS.length subBytes)
+
+    insertPreservingHighlights subBytes col = void $ nvim_input [i|<ESC>v#{col - start}hdi#{subBytes}|]
 
 cancelInput :: Neovim AgdaEnv ()
 cancelInput = asks symbolInputCol >>= \var -> atomically $ writeTVar var Nothing
