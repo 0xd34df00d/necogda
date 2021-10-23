ae6632c43aded816f1053a7aeb2c7e8cd1bb2a7f
0xd34df00d <0xd34df00d@gmail.com>
2021-10-23 17:35:02 -0500

    Properly account for unicode in highlights

--- src/Neovim/Agda/Dispatch.hs
+++ src/Neovim/Agda/Dispatch.hs
@@ -8,6 +8,8 @@ module Neovim.Agda.Dispatch
 ) where
 
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Control.Arrow
 import Control.Monad
 import Data.Foldable
@@ -30,22 +32,26 @@ stripMarker str
 parseResponse :: BS.ByteString -> Either String Response
 parseResponse = decodeResponse . stripMarker
 
-addHlBit :: Buffer -> [Int64] -> HlBit -> Neovim AgdaEnv ()
-addHlBit buf offsets (HlBit atoms [from, to])
+codepoint2byte :: T.Text -> Int64 -> Int64
+codepoint2byte line cp = fromIntegral $ BS.length $ T.encodeUtf8 $ T.take (fromIntegral cp) line
+
+addHlBit :: Buffer -> [Int64] -> [T.Text] -> HlBit -> Neovim AgdaEnv ()
+addHlBit buf offsets ls (HlBit atoms [from, to])
   | Just fromLine <- subtract 1 <$> findIndex (>= from) offsets
   , Just toLine <- subtract 1 <$> findIndex (>= to) offsets
     = let (fromOffset, toOffset) = join (***) (offsets !!) (fromLine, toLine)
           (fromLine', toLine') = join (***) fromIntegral (fromLine, toLine)
+          (fromStr, toStr) = join (***) (ls !!) (fromLine, toLine)
       in if fromLine' == toLine'
          then forM_ atoms $ \atom ->
-            nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (from - fromOffset - 1) (to - toOffset - 1)
+            nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (codepoint2byte fromStr $ from - fromOffset - 1) (codepoint2byte toStr $ to - toOffset - 1)
          else forM_ atoms $ \atom -> do
-            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (from - fromOffset - 1) (-1)
-            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] toLine' 0 (to - toOffset - 1)
+            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (codepoint2byte fromStr $ from - fromOffset - 1) (-1)
+            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] toLine' 0 (codepoint2byte toStr   $ to - toOffset - 1)
             forM_ [fromLine' + 1 .. toLine' - 1] $ \line ->
               nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] line 0 (-1)
   | otherwise = pure ()
-addHlBit _   _       (HlBit _     range) = nvim_err_writeln [i|Unexpected range format: #{range}|]
+addHlBit _   _       _  (HlBit _     range) = nvim_err_writeln [i|Unexpected range format: #{range}|]
 
 dispatchResponse :: Buffer -> Response -> Neovim AgdaEnv ()
 dispatchResponse _   (Status (StatusInfo checked _ _))
@@ -54,9 +60,9 @@ dispatchResponse _   (Status (StatusInfo checked _ _))
 dispatchResponse _   (InteractionPoints pts) = pure ()
 dispatchResponse _   (DisplayInfo di) = pure ()
 dispatchResponse buf (HighlightingInfo (HlInfo _ bits)) = do
-  ls <- nvim_buf_get_lines buf 0 (-1) False
-  let offsets = scanl (\acc str -> acc + fromIntegral (BS.length str) + 1) 0 $ toList ls
-  mapM_ (addHlBit buf offsets) bits
+  ls <- toList . fmap T.decodeUtf8 <$> nvim_buf_get_lines buf 0 (-1) False
+  let offsets = scanl (\acc str -> acc + fromIntegral (T.length str) + 1) 0 $ toList ls
+  mapM_ (addHlBit buf offsets ls) bits
 dispatchResponse _   (RunningInfo _ msg) = nvim_command [i|echom '#{msg}'|]
 dispatchResponse buf ClearHighlighting = nvim_buf_clear_namespace buf (-1) 0 (-1)
 dispatchResponse _   ClearRunningInfo = nvim_command "echo ''"
