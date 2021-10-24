2209bd03cb1fa5d6f710c58a1eb9eeb5faa5f207
0xd34df00d <0xd34df00d@gmail.com>
2021-10-24 12:59:35 -0500

    Encapsulate position to cursor mapping

--- src/Neovim/Agda/Dispatch.hs
+++ src/Neovim/Agda/Dispatch.hs
@@ -63,9 +63,8 @@ dispatchResponse ctx (DisplayInfo GoalSpecific { goalInfo = GoalInfo { .. } }) =
                                            reifyMarker = if originalName == reifiedName then "" else " (renamed to " <> reifiedName <> ")"
                                         in [i|#{originalName}#{reifyMarker}: #{binding}#{scopeMarker}|]
 dispatchResponse ctx (HighlightingInfo (HlInfo _ bits)) = do
-  ls <- toList . fmap T.decodeUtf8 <$> nvim_buf_get_lines (agdaBuffer ctx) 0 (-1) False
-  let offsets = scanl (\acc str -> acc + fromIntegral (T.length str) + 1) 0 $ toList ls
-  mapM_ (addHlBit (agdaBuffer ctx) offsets ls) bits
+  p2c <- preparePosition2Cursor $ agdaBuffer ctx
+  mapM_ (addHlBit (agdaBuffer ctx) p2c) bits
 dispatchResponse _   (RunningInfo _ msg) = nvim_command [i|echom '#{msg}'|]
 dispatchResponse ctx ClearHighlighting = nvim_buf_clear_namespace (agdaBuffer ctx) (-1) 0 (-1)
 dispatchResponse _   ClearRunningInfo = nvim_command "echo ''"
@@ -74,26 +73,41 @@ dispatchResponse _   JumpToError { .. } = pure ()
 codepoint2byte :: T.Text -> Int64 -> Int64
 codepoint2byte line cp = fromIntegral $ BS.length $ T.encodeUtf8 $ T.take (fromIntegral cp) line
 
-addHlBit :: Buffer -> [Int64] -> [T.Text] -> HlBit -> Neovim AgdaEnv ()
-addHlBit buf offsets ls (HlBit atoms [from, to])
-  | Just fromLine <- subtract 1 <$> findIndex (>= from) offsets
-  , Just toLine <- subtract 1 <$> findIndex (>= to) offsets
-    = let (fromOffset, toOffset) = join (***) (offsets !!) (fromLine, toLine)
-          (fromLine', toLine') = join (***) fromIntegral (fromLine, toLine)
-          (fromStr, toStr) = join (***) (ls !!) (fromLine, toLine)
-      in if fromLine' == toLine'
-         then forM_ atoms $ \atom ->
-            nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (codepoint2byte fromStr $ from - fromOffset - 1) (codepoint2byte toStr $ to - toOffset - 1)
-         else forM_ atoms $ \atom -> do
-            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine' (codepoint2byte fromStr $ from - fromOffset - 1) (-1)
-            void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] toLine' 0 (codepoint2byte toStr   $ to - toOffset - 1)
-            forM_ [fromLine' + 1 .. toLine' - 1] $ \line ->
-              nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] line 0 (-1)
+addHlBit :: Buffer -> Position2Cursor -> HlBit -> Neovim AgdaEnv ()
+addHlBit buf pos2cur (HlBit atoms [from, to])
+  | Just (fromLine, fromCol) <- position2cursor pos2cur from
+  , Just (toLine, toCol) <- position2cursor pos2cur to
+    = if fromLine == toLine
+      then forM_ atoms $ \atom ->
+         nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine fromCol toCol
+      else forM_ atoms $ \atom -> do
+         void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] fromLine fromCol (-1)
+         void $ nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] toLine   0       toCol
+         forM_ [fromLine + 1 .. toLine - 1] $ \line ->
+           nvim_buf_add_highlight buf (-1) [i|agda_atom_#{atom}|] line 0 (-1)
   | otherwise = pure ()
-addHlBit _   _       _  (HlBit _     range) = nvim_err_writeln [i|Unexpected range format: #{range}|]
+addHlBit _   _       (HlBit _     range) = nvim_err_writeln [i|Unexpected range format: #{range}|]
 
 setOutputBuffer :: Foldable f => DispatchContext -> f T.Text -> Neovim env ()
 setOutputBuffer ctx = nvim_buf_set_lines (outputBuffer ctx) 0 (-1) False
                     . V.concatMap (V.fromList . BS.split '\n' . T.encodeUtf8)
                     . V.fromList
                     . toList
+
+data Position2Cursor = Position2Cursor
+  { linesOffsets :: [Int64]
+  , linesContents :: [T.Text]
+  }
+
+preparePosition2Cursor :: Buffer -> Neovim AgdaEnv Position2Cursor
+preparePosition2Cursor buf = do
+  linesContents <- toList . fmap T.decodeUtf8 <$> nvim_buf_get_lines buf 0 (-1) False
+  let linesOffsets = scanl (\acc str -> acc + fromIntegral (T.length str) + 1) 0 $ toList linesContents
+  pure Position2Cursor { .. }
+
+position2cursor :: Position2Cursor -> Int64 -> Maybe (Int64, Int64)
+position2cursor Position2Cursor { .. } pos = do
+  lineIdx <- subtract 1 <$> findIndex (>= pos) linesOffsets
+  let lineOffset = linesOffsets !! lineIdx
+  let colIdx = codepoint2byte (linesContents !! lineIdx) (pos - lineOffset - 1)
+  pure (fromIntegral lineIdx, colIdx)
