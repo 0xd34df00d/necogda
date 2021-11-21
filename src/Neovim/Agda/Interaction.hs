07571ffc1cb78419aa6a033f598485dcc65ea6f5
0xd34df00d <0xd34df00d@gmail.com>
2021-11-21 13:18:06 -0600

    extmarks are byte-oriented, so consistently handle them as bytes

--- src/Neovim/Agda/Interaction.hs
+++ src/Neovim/Agda/Interaction.hs
@@ -8,6 +8,7 @@ module Neovim.Agda.Interaction
 , getCurrentInteractionId
 ) where
 
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Map as M
 import qualified Data.Text as T
@@ -19,6 +20,7 @@ import Data.Foldable
 import Data.Maybe
 import UnliftIO
 
+import qualified Neovim.API.Text as AT
 import Neovim
 import Neovim.API.ByteString
 
@@ -32,12 +34,14 @@ setInteractionMarks buffer pts = do
   goalmarksId <- asks goalmarksNs >>= readTVarIO
   nvim_buf_clear_namespace buffer goalmarksId 0 (-1)
 
+  ls <- AT.nvim_buf_get_lines (convertAPI buffer) 0 (-1) False
+
   id'range2markIds <- forM pts $ \RangeWithId { .. } -> do
     markIds <- forM range $ \Resp.Range { .. } -> do
       let startLine = line start - 1
-          startCol  = Resp.col start - 1
+          startCol  = ls V.! fromIntegral startLine @| Resp.col start - 1
           endLine = line end - 1
-          endCol  = Resp.col end - 1
+          endCol  = ls V.! fromIntegral endLine @| Resp.col end - 1
           extraOpts = M.fromList [ ("end_line", ObjectInt endLine)
                                  , ("end_col",  ObjectInt endCol)
                                  ]
@@ -51,8 +55,8 @@ setInteractionMarks buffer pts = do
   let id2marks = HM.fromList id'range2markIds
   pure (mark2id, id2marks)
 
-maybeText :: (T.Text -> Maybe T.Text) -> T.Text -> T.Text
-maybeText f txt = fromMaybe txt $ f txt
+maybeFallback :: (a -> Maybe a) -> a -> a
+maybeFallback f txt = fromMaybe txt $ f txt
 
 getCurrentInteractionId :: AgdaInstance -> Neovim AgdaEnv (Maybe (InteractionId, T.Text))
 getCurrentInteractionId AgdaInstance { .. } = do
@@ -64,16 +68,15 @@ getCurrentInteractionId AgdaInstance { .. } = do
   marks <- nvim_buf_get_extmarks buf goalmarksId (ObjectInt 0) (ObjectInt (-1)) [("details", ObjectBool True)]
   let extractLines (iid, (from, to)) = do
         ls <- nvim_buf_get_lines buf (fromIntegral $ row from) (fromIntegral $ row to + 1) False
-        let origText = T.strip $ runIdentity $ onRange from to $ extractLine (row from) ls
+        let origText = BS.strip $ runIdentity $ onRange from to $ extractLine (row from) ls
         let text = if origText == "?"
                    then mempty
-                   else T.strip $ maybeText (T.stripPrefix "{!") $ maybeText (T.stripSuffix "!}") origText
-        pure (iid, text)
+                   else BS.strip $ maybeFallback (BS.stripPrefix "{!") $ maybeFallback (BS.stripSuffix "!}") origText
+        pure (iid, T.decodeUtf8 text)
   traverse extractLines $ findMark (markId2interactionPoint payload) (curRow - 1) curCol marks
   where
-    extractLine start ls row maybeStart maybeEnd = pure $ maybe id T.drop maybeStart
-                                                        $ maybe id T.take maybeEnd
-                                                        $ T.decodeUtf8
+    extractLine start ls row maybeStart maybeEnd = pure $ maybe id BS.drop maybeStart
+                                                        $ maybe id BS.take maybeEnd
                                                         $ ls V.! (row - start)
 
 findMark :: Foldable f => MarkId2InteractionPoint -> Int64 -> Int64 -> f Object -> Maybe (InteractionId, (Cursor, Cursor))
