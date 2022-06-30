735d11fd81a2e263a3628d5e56f5f0982792381a
0xd34df00d <0xd34df00d@gmail.com>
2022-06-30 00:13:57 -0500

    Make goal window size width configurable

--- src/Neovim/Agda/Start.hs
+++ src/Neovim/Agda/Start.hs
@@ -99,9 +99,11 @@ startAgda = do
   buf <- vim_get_current_buffer
   name <- T.unpack . T.decodeUtf8 <$> buffer_get_name buf
 
+  goalWindowSize <- extractGoalWindowSize <$> nvim_eval [i|get(g:, 'necogda_goal_window_size', 60)|]
+
   nvim_command [i|vertical belowright pedit! Goals|]
   nvim_command [i|wincmd P|]
-  nvim_command [i|60wincmd >|]
+  nvim_command [i|#{goalWindowSize}wincmd >|]
   nvim_command [i|setlocal buftype=nofile nobuflisted bufhidden=wipe|]
   outputBuffer <- vim_get_current_buffer
   nvim_command [i|wincmd p|]
@@ -131,3 +133,8 @@ startAgda = do
            watchErrors nvim_err_writeln inst
            watchStdout parseDispatch nvim_err_writeln inst
            loadFile
+  where
+    extractGoalWindowSize :: Object -> Int
+    extractGoalWindowSize (ObjectInt n) = fromIntegral n
+    extractGoalWindowSize (ObjectUInt n) = fromIntegral n
+    extractGoalWindowSize _ = 60
