b4ffae74aa42f3ffe2bb6eb300ba29a745041d7a
0xd34df00d <0xd34df00d@gmail.com>
2024-05-04 11:05:58 -0500

    Use `case` with proper branches instead of `unless`/`head`
    The former is more obviously total and thus avoids yet another warning
    from the compiler.

--- src/Neovim/Agda/Request/NvimActions.hs
+++ src/Neovim/Agda/Request/NvimActions.hs
@@ -7,7 +7,6 @@ module Neovim.Agda.Request.NvimActions
 
 import qualified Data.Vector as V
 import Control.Arrow
-import Control.Monad
 import Data.List
 import Data.Maybe
 import Data.Ord
@@ -24,10 +23,12 @@ goalNav sorter cmpWith = do
   buf <- nvim_get_current_buf
   win <- nvim_get_current_win
   marks <- sortOn sorter . V.toList <$> getGoalMarks buf
-  unless (null marks) $ do
-    cur <- uncurry Cursor . first (subtract 1) <$> nvim_win_get_cursor win
-    let nextCur = markStart $ fromMaybe (head marks) $ find (cmpWith cur) marks
-    nvim_win_set_cursor win $ first (+ 1) $ curToPair nextCur
+  case marks of
+    [] -> pure ()
+    (mark : _) -> do
+      cur <- uncurry Cursor . first (subtract 1) <$> nvim_win_get_cursor win
+      let nextCur = markStart $ fromMaybe mark $ find (cmpWith cur) marks
+      nvim_win_set_cursor win $ first (+ 1) $ curToPair nextCur
 
 goalNext, goalPrev :: Neovim AgdaEnv ()
 goalNext = goalNav id   $ \cur markObj -> markStart markObj > cur
